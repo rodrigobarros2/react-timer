32c2f0c91586c7f92d4498fe2fb2bd774bb19ae1
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-01-20 11:39:31 -0300

    aaa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './App'; //importei o componente Social de "./App"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); //Social Importado agora Renderizo o Componente em Root
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
